cfd445616230e9be1aa92492319bb69f126cc27e
Rajasekhar Reddy Ankireddypalli <rajachandu4959@gmail.com>
2023-11-20 08:25:53 +0530

    Added definitions

--- BasicPrograms/filter_lambda.py
+++ BasicPrograms/filter_lambda.py
@@ -1,3 +1,10 @@
+# reduce method:
+# Takes two elements from a sequence and replace them with single value returned by the method given
+# seq = [1,5,7,9]
+# reduce(lambda a,b : a+b, seq)
+# [1,5,7,9] --> [6,7,9] --> [13,9] --> 22
+
+# filter method return a new seq with filtered elements using the method.
 
 from functools import reduce
 
@@ -15,5 +22,5 @@ for a in numbers:
 
 print(sum)
 
-total = reduce(lambda a,b : a+b, numbers)
+total = reduce(lambda a, b: a+b, numbers)
 print(total)
